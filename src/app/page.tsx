061c0b2c325b3912bc522516e0c0b42dd6ba34ad
masayahak <masayahak@gmail.com>
2026-01-11 11:13:27 +0900

    Refactor main page to utilize TodoApp component and fetch todos; remove unused x_page.tsx file and update Prisma client import path.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import { getAllTodos } from "@/lib/todo";
+import { TodoApp } from "@/components/TodoApp";
+
 export default async function Home() {
+  const todos = await getAllTodos();
+
   return (
-    <div>
-      <h1>Amplify Test: Hello World</h1>
-    </div>
+    <main className="min-h-screen flex items-center justify-center p-4 bg-background">
+      {/* 重要：コンポーネントTodoAppを todosへ依存させる
+                つまりtodosが変化したら、TodoAppは再描画される */}
+      <TodoApp todos={todos} />
+    </main>
   );
 }
