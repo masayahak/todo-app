c22b160aeedef7e996a46faa11e84db80a08fd6e
masayahak <masayahak@gmail.com>
2026-01-11 11:47:23 +0900

    Prismaを安定版6.2.1へ変更

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,15 @@
-import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
+import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient;
 };
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-});
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
 
 const prisma =
   globalForPrisma.prisma ||
